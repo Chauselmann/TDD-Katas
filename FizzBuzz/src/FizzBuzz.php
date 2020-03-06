335f9f02bf2478684f0ebcd17a058456a72762a0
chauselmann <charlie@cosavostra.com>
2020-03-06 15:05:55 +0100

    REFACTO : Should return Fizz when divisible by 3

--- FizzBuzz/src/FizzBuzz.php
+++ FizzBuzz/src/FizzBuzz.php
@@ -7,6 +7,7 @@ class FizzBuzz {
     }
 
     public function renderFizzBuzz(int $param){
+        //if divisible by 3 return Fizz
         if ($param % 3 == 0){
             return 'Fizz';
         } else {
