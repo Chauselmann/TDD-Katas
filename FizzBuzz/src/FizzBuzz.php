0ace9a267c75dd551711bba8b806f2188ed534be
chauselmann <charlie@cosavostra.com>
2020-03-06 15:17:57 +0100

    REFACTO : Should return FizzBuzz when divisible by 15

--- FizzBuzz/src/FizzBuzz.php
+++ FizzBuzz/src/FizzBuzz.php
@@ -7,15 +7,14 @@ class FizzBuzz {
     }
 
     public function renderFizzBuzz(int $param){
-
+        // if divisible by 15 return FizzBuzz
         if($param % 15 == 0){
             return 'FizzBuzz';
         } elseif ($param % 3 == 0){ //if divisible by 3 return Fizz
             return 'Fizz';
         } elseif ($param % 5 == 0){ //if divisible by 5 return Buzz
             return 'Buzz';
-        }
-        else {
+        } else { //return the number
             return $param;
         }
     }
